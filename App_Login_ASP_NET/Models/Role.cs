237c4d22e6228fba5f9f8754a7b35d52b9a0519f
PABLO VALENTIN <pablo.valentin@fatec.sp.gov.br>
2025-05-11 19:21:38 -0300

    Definindo os atributos das classes User e Role.

--- App_Login_ASP_NET/Models/Role.cs
+++ App_Login_ASP_NET/Models/Role.cs
@@ -1,10 +1,13 @@
-﻿namespace App_Login_ASP_NET.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App_Login_ASP_NET.Models
 {
 
     public class Role
     {
 
-
+        [Required]
+        public string? Nome { get; set; }
 
     }
 
--- App_Login_ASP_NET/Models/User.cs
+++ App_Login_ASP_NET/Models/User.cs
@@ -1,10 +1,26 @@
-﻿namespace App_Login_ASP_NET.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace App_Login_ASP_NET.Models
 {
 
     public class User
     {
 
+        [Required]
+        public string? Name { get; set; }
 
+        [Required]
+        [EmailAddress(ErrorMessage = "Todo usuário precisa ter um e-mail associado a ele.")]
+        public string? Email { get; set; }
+
+        [Required]
+        [Phone]
+        public string? Phone { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
 
     }
 
